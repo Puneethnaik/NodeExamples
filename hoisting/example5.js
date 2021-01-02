8ee3dec572bea2503d91a2491bde8d74bf27df10
Puneeth N Naik <puneethn250@gmail.com>
2021-01-02 18:35:28 +0530

    temporal dead zone for a const variable

--- hoisting/example5.js
+++ hoisting/example5.js
@@ -1,10 +1,10 @@
 let a = 10;
-// console.log(c);
+console.log(c);
 const c = 10;
 console.log(c);
-c = 20;
+// c = 20;
 
 /**
- * Temporal dead zone not allowed for a const variable.
- * That is, a const variable must be initialised.
+ * Temporal dead zone allowed for a const variable.
+ * But, a const variable must be initialised.
  */
