17a7dc989d0f11fc49d0461b5dfce42af8271272
Puneeth N Naik <puneethn250@gmail.com>
2021-01-02 18:33:47 +0530

    demonstration of temporal dead zone of let variables

---
+++ hoisting/example4.js
@@ -0,0 +1,12 @@
+console.log(b);
+let b = 6;
+
+let c;
+console.log(c);
+c = 6;
+
+/**
+ * Temporal dead zone for c from lines 1 to line 4
+ * variable undefined from first declaration and first definition(assignment)
+ * Temporal dead zone is the region in which the variable is unintiliazed.
+ */
